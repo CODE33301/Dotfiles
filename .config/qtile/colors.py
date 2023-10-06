a6d3a019b379220af50094b1c21160e16e8e8cce
Amparo R <46226136+CODE33301@users.noreply.github.com>
2023-10-05 21:28:10 -0500

    Create colors.py

---
+++ .config/qtile/colors.py
@@ -0,0 +1,16 @@
+purpleTheme = {
+    "borderNormal":"#2A0080",
+    "borderFocus":"#9966FF",
+}
+
+orangeTheme = {
+    "borderNormal":"#803300",
+    "borderFocus":"#FF6600",
+}
+
+blueTheme = {
+    "borderNormal":"#003380",
+    "borderFocus":"#0066FF",
+}
+
+selectedTheme = purpleTheme
