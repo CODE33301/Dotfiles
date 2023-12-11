1bb4e0066708cb3af4be21849398061e3605c5e6
Amparo R <46226136+CODE33301@users.noreply.github.com>
2023-12-11 13:04:14 -0600

    Update rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -284,8 +284,10 @@ globalkeys = gears.table.join(
     -- Prompt
     awful.key({ modkey }, "r", function () awful.screen.focused().mypromptbox:run() end, {description = "run prompt", group = "launcher"}),
     awful.key({ modkey }, "x", function () awful.prompt.run { prompt = "Run Lua code: ", textbox = awful.screen.focused().mypromptbox.widget, exe_callback = awful.util.eval, history_path = awful.util.get_cache_dir() .. "/history_eval" } end, {description = "lua execute prompt", group = "awesome"}),
-    -- Menubar
-    awful.key({ modkey }, "p", function() menubar.show() end, {description = "show the menubar", group = "launcher"})
+    -- Menubar Default
+    --awful.key({ modkey }, "p", function() menubar.show() end, {description = "show the menubar", group = "launcher"})
+    -- Menubar With Rofi
+    awful.key({ modkey }, "p", function() awful.spawn("rofi -show run") end, {description = "show the menubar", group = "launcher"})
 )
 clientkeys = gears.table.join(
     awful.key({ modkey, }, "f", function (c) c.fullscreen = not c.fullscreen c:raise() end, {description = "toggle fullscreen", group = "client"}),
