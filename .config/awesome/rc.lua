8628a061c90219f1e2559602cbafcf2b0b4c3565
Amparo R <46226136+CODE33301@users.noreply.github.com>
2023-12-12 21:24:57 -0600

    Update rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -454,3 +454,5 @@ end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+
+awful.spawn.with_shell("feh --bg-scale /home/$USER/.config/Wallpapers/JPG/_DSC6639.jpg")
